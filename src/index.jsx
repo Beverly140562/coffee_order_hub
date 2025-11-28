707cf66c45dc6617a82d5ab8d7a51e1425190f02
Beverly140562 <beverlysalde.140562@gmail.com>
2025-11-28 20:11:25 +0800

    Initial PWA commit

---
+++ src/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import React from "react/client";
+import { BrowserRouter as Router } from "react-router";
+import App from "./App";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+);
+
+// Register service worker for PWA
+serviceWorker.register();
