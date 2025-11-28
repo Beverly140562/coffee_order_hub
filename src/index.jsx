5f1fece6740bcccdc06ad605f211034695dd5a78
Beverly140562 <beverlysalde.140562@gmail.com>
2025-11-28 21:00:38 +0800

    .

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,15 @@ import React from "react/client";
 import { BrowserRouter as Router } from "react-router";
 import App from "./App";
 import "./index.css";
-import * as serviceWorker from "../public/serviceWorker";
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/serviceWorker.js')
+      .then(() => console.log('Service Worker registered'))
+      .catch(err => console.error('SW registration failed:', err));
+  });
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -16,4 +24,5 @@ root.render(
 
 // Register service worker for PWA
 serviceWorker.register();
+
 
